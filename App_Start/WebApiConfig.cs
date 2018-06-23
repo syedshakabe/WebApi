8b1eeabde997e35dfd709fbb5074d926a40f8b79
shakabe <bitf14m037@pucit.edu.pk>
2018-06-23 20:27:31 +0500

    login5

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,67 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.Owin.Security.OAuth;
+using Newtonsoft.Json.Serialization;
+using System.Web.Http.Cors;
+using System.Web.Http.Routing;
+
+namespace WebApplication5
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            // Configure Web API to use only bearer token authentication.
+            config.SuppressDefaultHostAuthentication();
+            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute("DefaultApiWithId", "Api/{controller}/{id}", new { id = RouteParameter.Optional }, new { id = @"\d+" });
+            config.Routes.MapHttpRoute("DefaultApiWithAction", "Api/{controller}/{action}");
+            config.Routes.MapHttpRoute("DefaultApiGet", "Api/{controller}", new { action = "Get" }, new { httpMethod = new HttpMethodConstraint(HttpMethod.Get) });
+            config.Routes.MapHttpRoute("DefaultApiPost", "Api/{controller}", new { action = "Post" }, new { httpMethod = new HttpMethodConstraint(HttpMethod.Post) });
+
+
+                        //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional }
+            //);
+
+
+
+//            config.Routes.MapHttpRoute(
+//    name: "ActionApi",
+//    routeTemplate: "api/{controller}/{action}/{id}",
+//    defaults: new { id = RouteParameter.Optional }
+//);
+
+
+
+
+          
+
+
+
+
+
+ //EnableCorsAttribute cors = new EnableCorsAttribute("*", "*","*");
+            config.EnableCors();
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+           // config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+           // config.Formatters.JsonFormatter.SerializerSettings.ContractResolver =  new CamelCasePropertyNamesContractResolver();
+
+            //added for order
+            //var json = config.Formatters.JsonFormatter;
+            //json.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects;
+            config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+
+        }
+    }
+}
---
+++ Models/firstPagesController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication5.Models
+{
+    [Authorize(Roles = "admin")]
+    public class firstPagesController : Controller
+    {
+        
+        private projectEntities4 db = new projectEntities4();
+
+        // GET: firstPages
+     
+        public ActionResult Index()
+        {
+            return View(db.firstPages.ToList());
+        }
+
+        // GET: firstPages/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            firstPage firstPage = db.firstPages.Find(id);
+            if (firstPage == null)
+            {
+                return HttpNotFound();
+            }
+            return View(firstPage);
+        }
+
+        // GET: firstPages/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: firstPages/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,src,description,price")] firstPage firstPage)
+        {
+            if (ModelState.IsValid)
+            {
+                db.firstPages.Add(firstPage);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(firstPage);
+        }
+
+        // GET: firstPages/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            firstPage firstPage = db.firstPages.Find(id);
+            if (firstPage == null)
+            {
+                return HttpNotFound();
+            }
+            return View(firstPage);
+        }
+
+        // POST: firstPages/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,src,description,price")] firstPage firstPage)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(firstPage).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(firstPage);
+        }
+
+        // GET: firstPages/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            firstPage firstPage = db.firstPages.Find(id);
+            if (firstPage == null)
+            {
+                return HttpNotFound();
+            }
+            return View(firstPage);
+        }
+
+        // POST: firstPages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            firstPage firstPage = db.firstPages.Find(id);
+            db.firstPages.Remove(firstPage);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
